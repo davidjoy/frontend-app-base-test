e6999c137bedaef54d29254e72b096ba8ece68a8
David Joy <davjoy@gmail.com>
2024-06-27 22:42:02 -0400

    fix: adding a FormattedMessage so we can test i18n_extract

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,3 +1,4 @@
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import { Container } from '@openedx/paragon';
 
 const ExamplePage = () => (
@@ -5,6 +6,11 @@ const ExamplePage = () => (
     <Container className="py-5">
       <h1>Example Page</h1>
       <p>Hello world!</p>
+      <FormattedMessage
+        id="example.helloWorld"
+        defaultMessage="Hello world!"
+        description="A generic hello world message."
+      />
     </Container>
   </main>
 );
