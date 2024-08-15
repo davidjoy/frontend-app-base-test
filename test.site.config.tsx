39d39850663bbcb186f94c2fb3bb1bad51579694
David Joy <davjoy@gmail.com>
2024-08-14 22:19:39 -0400

    fix: adding build tsconfig, adding test site config, fleshing out tsconfig.json
    - Adds a new tsconfig.build.json specifically for building this repo as a library
    - Updates tsconfig.json to include the tool config files and such, since it’s primarily used for IDEs and such.
    - Adding a test.site.config.tsx file for the test suite (once it exists)

---
+++ test.site.config.tsx
@@ -0,0 +1,35 @@
+import { SiteConfig } from '@openedx/frontend-base';
+
+const config: SiteConfig = {
+  ACCESS_TOKEN_COOKIE_NAME: 'edx-jwt-cookie-header-payload',
+  BASE_URL: 'http://localhost:8080',
+  ACCOUNT_PROFILE_URL: 'http://localhost:1995',
+  CREDENTIALS_BASE_URL: 'http://localhost:18150',
+  CSRF_TOKEN_API_PATH: '/csrf/api/v1/token',
+  ECOMMERCE_BASE_URL: 'http://localhost:18130',
+  LANGUAGE_PREFERENCE_COOKIE_NAME: 'openedx-language-preference',
+  LMS_BASE_URL: 'http://localhost:18000',
+  LOGIN_URL: 'http://localhost:18000/login',
+  LOGOUT_URL: 'http://localhost:18000/logout',
+  LOGO_URL: 'https://edx-cdn.org/v3/default/logo.svg',
+  LOGO_TRADEMARK_URL: 'https://edx-cdn.org/v3/default/logo-trademark.svg',
+  LOGO_WHITE_URL: 'https://edx-cdn.org/v3/default/logo-white.svg',
+  FAVICON_URL: 'https://edx-cdn.org/v3/default/favicon.ico',
+  MARKETING_SITE_BASE_URL: 'http://localhost:18000',
+  ORDER_HISTORY_URL: 'http://localhost:1996/orders',
+  REFRESH_ACCESS_TOKEN_ENDPOINT: 'http://localhost:18000/login_refresh',
+  SEGMENT_KEY: '',
+  SITE_NAME: 'localhost',
+  USER_INFO_COOKIE_NAME: 'edx-user-info',
+  APP_ID: 'base-test',
+  ENVIRONMENT: 'dev',
+  ACCOUNT_SETTINGS_URL: 'http://localhost:1997',
+  DISCOVERY_API_BASE_URL: 'http://localhost:18381',
+  IGNORED_ERROR_REGEX: null,
+  LEARNING_BASE_URL: 'http://localhost:2000',
+  PUBLIC_PATH: '/',
+  PUBLISHER_BASE_URL: 'http://localhost:18400',
+  STUDIO_BASE_URL: 'http://localhost:18010',
+};
+
+export default config;
